5b55d94ba05b62a2b8d2d240ca775badb54ba426
jzeuner <jzeuner.oss@gmail.com>
2019-09-28 19:30:50 +0200

    Flake8 and went over format.py

--- Formats/format.py
+++ Formats/format.py
@@ -1,17 +1,21 @@
 from .csv_format import csv_toTree, csv_fromTree
 from .txt_format import txt_toTree, txt_fromTree
 
+import types
+
 # Supported File Formats
 
 TYPES = {'.csv': ('csv', [';', ','], csv_toTree, csv_fromTree),
          '.txt': ('txt', [';', ','], txt_toTree, txt_fromTree)}
 
+
 class Format:
     """
-    Class that represents and handles different file formats.
+    Class that can represent and handle different file formats.
 
     For the set format, it can load a file into an internal data structure.
-    And also parse the previous created internal structure into the set ouptut format.
+    And also parse the previous created internal structure into the set ouptut
+    format.
     """
 
     def __init__(self, name, delimiters=[], loader=None, parser=None):
@@ -25,52 +29,69 @@ class Format:
         delimiters: list
             List of all supported delimiters
         loader: function
-            loader function that loads a file of the format into an internal data structure
+            loader function that loads a file of the format into an internal
+            data structure
         parser: function
-            parser function that parses an internal data structure into a file of the format
+            parser function that parses an internal data structure into a file
+            of the format
         """
 
         self.name = name
         self.delimiters = delimiters
+
+        if not isinstance(loader, types.FunctionType):
+            raise TypeError('loader must be a function')
+
+        if not isinstance(parser, types.FunctionType):
+            raise TypeError('parser must be a function')
+
         self.loader = loader
         self.parser = parser
 
-
     def load(self, file, delimiter=None):
         """
-        Uses the loader function to load a file into the internal data structure.
+        Uses the loader function to load a file into the internal data
+        structure.
 
         Parameters
         ----------
         file: file
-        delimiter: list, optional
+        delimiter: str, optional
+            If delimiter is none the first format delimiter is used
 
         Returns
         -------
         content: DataTree
         """
 
-        if delimiter != None:
-            return self.loader(file, delimiter)
+        if delimiter is None:
+            return self.loader(file, self.delimiters[0])
 
-        return self.loader(file, self.delimiters[0])
+        if not isinstance(delimiter, str):
+            raise TypeError('delimiter must be a str')
 
+        return self.loader(file, delimiter)
 
     def parse(self, content, file, delimiter=None):
         """
-        Uses the parser function to parse the internal data structure into the format.
+        Uses the parser function to parse the internal data structure into the
+        format.
 
         Parameters
         ----------
         content: DataTree
         file: file
-        delimiter: list, optional
+        delimiter: str, optional
+            If delimiter is none the first format delimiter is used
         """
 
-        if delimiter != None:
-            self.parser(content, file, delimiter)
-        else:
+        if delimiter is None:
             self.parser(content, file, self.delimiters[0])
+        else:
+            if not isinstance(delimiter, str):
+                raise TypeError('delimiter must be a str')
+
+            self.parser(content, file, delimiter)
 
 
 def checkTypes(type_names=[]):
@@ -96,12 +117,16 @@ def getFormats(type_names=[]):
     Parameters
     ----------
     type_names: list
+        each type is a str and looks like this '.type'
 
     Returns
     -------
     types: list
         list of Type Objects
     """
+
+    if not isinstance(type_names, list):
+        raise TypeError('type_names must be a list')
 
     checkTypes(type_names)
 
